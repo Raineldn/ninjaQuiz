4c88350d6e7d5979aff26990354c90a97177e72e
raineldn <raineldn@gmail.com>
2019-05-20 13:14:25 +0100

    Added score animation and finishing touches

--- app.js
+++ app.js
@@ -15,9 +15,23 @@ form.addEventListener('submit', e => {
 		}
 	});
 
-	result.querySelector('span').textContent = `${score}%`;
-	result.classList.remove('d-none');
+	// show result
 
 	scrollTo(0,0);
+
+	result.classList.remove('d-none');
+
+	// score animation
+	let output = 0;
+	const timer = setInterval(() => {
+		result.querySelector('span').textContent = `${output}%`;
+		if(output === score){
+			clearInterval(timer);
+		} else {
+			output++;
+		}
+	}, 10);
+
 });
+
 
